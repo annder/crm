04a6f0bd1162829835330ef180552fe6af42b18b
annter <15623317087@163.com>
2020-06-02 17:38:09 +0800

    封装sibar

--- src/App.js
+++ src/App.js
@@ -35,36 +35,79 @@ function Dashboard() {
   );
 }
 
-/**
- * @param {String}  title
- * @param {import('react').ReactElement} icon
- * @param {Array} items
- * */
-
-const SubMenuSetting = [
+const SibarItems = [
   {
-    name: 'text',
-    icon: '',
-    children: [{ name: '', icon: '' }],
+    title: '管理系统',
+    children: [
+      {
+        title: '部门管理',
+      },
+      {
+        title: '权限管理',
+      },
+      {
+        title: '角色管理',
+      },
+    ],
   },
-  {},
+  {
+    title: '1',
+  },
 ];
 
-/**
- * @param {Array} subItems
- * */
+const RouterConfig = () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/dashboard">
+        <Dashboard />
+      </Route>
+    </Switch>
+  );
+};
 
-const SibarItem = ({ subItems }) => {
-  const { Item } = Menu;
-  // 下面的Item
-  const SubItem = (items) =>
-    items.map((item, index) => <Item key={index}> {item.name}</Item>);
-
-  return subItems.map((item) => (
-    <SubMenu key="sub1" icon={<item.icon />} title={title}>
-      <SubItem items={imte.children}></SubItem>
-    </SubMenu>
-  ));
+const MainContent = () => (
+  <Layout style={{ padding: '0 24px 24px' }}>
+    <Content
+      className="site-layout-background"
+      style={{
+        padding: 24,
+        margin: 0,
+        minHeight: 280,
+      }}
+    >
+      <RouterConfig />
+    </Content>
+  </Layout>
+);
+const SibarMenu = () => {
+  return (
+    <Menu
+      mode="inline"
+      defaultSelectedKeys={['1']}
+      defaultOpenKeys={['sub1']}
+      style={{ height: '100%', borderRight: 0 }}
+    >
+      {SibarItems.map((element, index) =>
+        element.children ? (
+          <SubMenu key={index} title={element.title}>
+            {element.children
+              ? element.children.map((subItem, subIndex) => (
+                  <Menu.Item key={subIndex}>{subItem.title}</Menu.Item>
+                ))
+              : null}
+          </SubMenu>
+        ) : (
+          <Menu.Item key={index}>{element.title}</Menu.Item>
+        )
+      )}
+    </Menu>
+  );
 };
 
 function App() {
@@ -77,63 +120,9 @@ function App() {
           </Header>
           <Layout className="layout-main">
             <Sider width={200} className="site-layout-background">
-              <Menu
-                mode="inline"
-                defaultSelectedKeys={['1']}
-                defaultOpenKeys={['sub1']}
-                style={{ height: '100%', borderRight: 0 }}
-              >
-                <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
-                  <Menu.Item key="1">option1</Menu.Item>
-                  <Menu.Item key="2">option2</Menu.Item>
-                  <Menu.Item key="3">option3</Menu.Item>
-                  <Menu.Item key="4">option4</Menu.Item>
-                </SubMenu>
-                <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
-                  <Menu.Item key="5">option5</Menu.Item>
-                  <Menu.Item key="6">option6</Menu.Item>
-                  <Menu.Item key="7">option7</Menu.Item>
-                  <Menu.Item key="8">option8</Menu.Item>
-                </SubMenu>
-                <SubMenu
-                  key="sub3"
-                  icon={<NotificationOutlined />}
-                  title="subnav 3"
-                >
-                  <Menu.Item key="9">option9</Menu.Item>
-                  <Menu.Item key="10">option10</Menu.Item>
-                  <Menu.Item key="11">option11</Menu.Item>
-                  <Menu.Item key="12">option12</Menu.Item>
-                </SubMenu>
-              </Menu>
+              <SibarMenu />
             </Sider>
-            <Layout style={{ padding: '0 24px 24px' }}>
-              <Breadcrumb style={{ margin: '16px 0' }}>
-                <Breadcrumb.Item>Home</Breadcrumb.Item>
-                <Breadcrumb.Item>List</Breadcrumb.Item>
-                <Breadcrumb.Item>App</Breadcrumb.Item>
-              </Breadcrumb>
-              <Content
-                className="site-layout-background"
-                style={{
-                  padding: 24,
-                  margin: 0,
-                  minHeight: 280,
-                }}
-              >
-                <Switch>
-                  <Route exact path="/">
-                    <Home />
-                  </Route>
-                  <Route path="/about">
-                    <About />
-                  </Route>
-                  <Route path="/dashboard">
-                    <Dashboard />
-                  </Route>
-                </Switch>
-              </Content>
-            </Layout>
+            <MainContent />
           </Layout>
         </Layout>
       </Router>
